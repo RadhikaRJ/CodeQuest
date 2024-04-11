5a38812e13256f6c01163cd76027e6d11ca284b2
Radhika Joshi <radhika7002@gmail.com>
2024-04-11 08:51:20 -0700

    Time: 0 ms (100%), Space: 45.8 MB (62.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,53 +1,49 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-        int[] result = {-1,-1};
-        if(nums.length==0||nums[0]>target||nums[n-1]<target){ //take care of these edge cases initially.
+        int[] result = { -1, -1 };
+        if (nums.length == 0 || nums[0] > target || nums[n - 1] < target) { // take care of these edge cases initially.
             return result;
         }
-        
-        result[0] = findFirstOccurrence(nums,target);
-        result[1] = findLastOccurrence(nums,target);
+
+        result[0] = findFirstOccurrence(nums, target);
+        result[1] = findLastOccurrence(nums, target);
 
         return result;
 
-
-
     }
 
-    private int findLastOccurrence(int[] nums, int target){
+    private int findLastOccurrence(int[] nums, int target) {
         int n = nums.length;
-        int left = 0, right = n-1;
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            if(nums[mid]>target){
-                right=mid-1;
-            }
-            else{
-                left=mid+1;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-       
-        return nums[right]==target?right:-1;
+
+        return nums[right] == target ? right : -1;
     }
 
-    private int findFirstOccurrence(int[] nums, int target){
+    private int findFirstOccurrence(int[] nums, int target) {
         int n = nums.length;
-        int left = 0, right = n-1;
-        
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            if(nums[mid]>=target){
-                right = mid-1;
-            }
-            else{
-                left=mid+1;
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        if(left<0){
+        if (left < 0) {
             return -1;
         }
-        return nums[left]==target?left:-1;
+        return nums[left] == target ? left : -1;
 
     }
 }
