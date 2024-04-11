fc53c2b6055c75cda10a81e276dae0bbebec533c
Radhika Joshi <radhika7002@gmail.com>
2024-04-11 16:09:23 -0700

    Time: 0 ms (100%), Space: 42.4 MB (48.52%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,27 +9,22 @@
  * }
  */
 class Solution {
-    ListNode newHead = null;
+    /**
+    1->2->3->4->5
+    head = 4 |3 |2 |1
+    node = 5| 5->4 | 5->4->3->null | 5->4->3->2->null
+    head.next.next = 4->5->4 | 3->4->3 => 5->4->3->4 | 2->3->2 => 5->4->3->2->3 | 1->2->1=>5->4->3->2->1->2
+    head.next=null = 4->null | 5->4 | 5->4->3->null | 5->4->3->2->null|5->4->3->2->1->null
+    return node | 5->4 | 5->4->3 | 5->4->3->2 | 5->4->3->2->1->
+     */
     public ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        reverse(head);
-        head.next = null;
-        return newHead;
-    }
-
-    public ListNode reverse(ListNode head) {
-        if (head == null) {
-            return null;
+        if(head==null || head.next==null){
+            return head;
         }
 
-        ListNode nextNode = reverse(head.next);
-        if (nextNode != null) {
-            nextNode.next = head; 
-        } else {
-            newHead = head;
-        }
-        return head;
+        ListNode node = reverseList(head.next);
+        head.next.next = head; //important concept: 1->2->1
+        head.next = null;//2->1->null
+        return node;
     }
 }
