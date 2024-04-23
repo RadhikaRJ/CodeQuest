1d79a39a8119bc5a86b812415fd20eb7852eb8df
Radhika Joshi <radhika7002@gmail.com>
2024-04-23 11:49:56 -0700

    Time: 1 ms (21.23%), Space: 44.8 MB (17.46%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,26 +14,48 @@
  * }
  */
 class Solution {
-    int maxDiameterLength = 0;
+    int diameterLength = Integer.MIN_VALUE;
     public int diameterOfBinaryTree(TreeNode root) {
-        calculateDiameter(root);
-        return maxDiameterLength-1;
-    }
-
-    public int calculateDiameter(TreeNode root){
         if(root==null){
             return 0;
         }
-        if(root.left==null && root.right==null){
+        int val = findDiameter(root);
+        return diameterLength==Integer.MIN_VALUE?0:diameterLength;
+    }
+
+    public int findDiameter(TreeNode root){
+
+        /**         4
+                2       5
+         */
+        if(root==null){
+            return 0;
+        }
+
+        if(root.left == null && root.right == null){
             return 1;
         }
 
-        int leftDiameter = 1+calculateDiameter(root.left);
-        maxDiameterLength = Math.max(maxDiameterLength,leftDiameter);
-        int rightDiameter = 1+calculateDiameter(root.right);
-        maxDiameterLength = Math.max(maxDiameterLength,rightDiameter)-1;
-        maxDiameterLength = Math.max(maxDiameterLength,leftDiameter+rightDiameter-1);
-        return Math.max(leftDiameter,rightDiameter);
+        int leftPathLength = findDiameter(root.left); //fd(2)=1
+        int rightPathLength = findDiameter(root.right); //fd(5)=1
 
+        int pathWithRoot = leftPathLength+rightPathLength;  //2
+
+        diameterLength = Math.max(diameterLength, leftPathLength); //2
+        diameterLength = Math.max(diameterLength, rightPathLength);//2
+        diameterLength = Math.max(diameterLength, pathWithRoot);//2
+  
+        
+        return 1+Math.max(leftPathLength,rightPathLength); //1
     }
 }
+
+
+            //             2
+            //         1       3
+            //     4       7
+            // 5               8
+            //                     9
+            //                         10
+        
+
