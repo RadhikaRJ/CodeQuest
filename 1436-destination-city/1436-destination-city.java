19a075d3a1c4f65a320a7bc1a0b9ad084de667e0
Radhika Joshi <radhika7002@gmail.com>
2024-05-08 12:03:18 -0700

    Time: 2 ms (99.54%), Space: 42.9 MB (77.74%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -1,25 +1,39 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
        
-       Map<String,String> map = new HashMap<>();
-       String finalDestination = "";
-        for(List<String> path:paths){
-            String source = path.get(0);
-            String destination = path.get(1);
-            map.put(source, destination);
+    //    Map<String,String> map = new HashMap<>();
+    //    String finalDestination = "";
+    //     for(List<String> path:paths){
+    //         String source = path.get(0);
+    //         String destination = path.get(1);
+    //         map.put(source, destination);
+    //     }
+
+    //     for(List<String> path:paths){
+    //         String destination = path.get(1);
+    //         while(map.containsKey(destination)){
+    //             destination = map.get(destination);
+    //         }
+    //         finalDestination = destination;
+    //         break;
+
+    //     }
+
+    //    return finalDestination;
+
+    Set<String> hasOutGoing = new HashSet<>();
+    for(int i=0;i<paths.size();i++){
+        hasOutGoing.add(paths.get(i).get(0)); //add the sources to the set
+    }
+
+    for(int i=0;i<paths.size();i++){
+        String dest = paths.get(i).get(1);
+        if(!hasOutGoing.contains(dest)){
+            return dest;
         }
+    }
 
-        for(List<String> path:paths){
-            String destination = path.get(1);
-            while(map.containsKey(destination)){
-                destination = map.get(destination);
-            }
-            finalDestination = destination;
-            break;
-
-        }
-
-       return finalDestination;
+    return "";
 
     }
 }
