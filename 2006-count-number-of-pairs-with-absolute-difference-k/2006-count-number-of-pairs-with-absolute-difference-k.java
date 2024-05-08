e7a44265925852febe84d737e052e3c02f89aa7f
Radhika Joshi <radhika7002@gmail.com>
2024-05-08 11:15:03 -0700

    Time: 1 ms (97.16%), Space: 43.8 MB (32.39%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,14 +1,28 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
         int pairCount = 0;
-        int n =nums.length;
-        for(int i=0;i<n-1;i++){
-            for(int j = i+1;j<n;j++){
-                if(Math.abs(nums[i]-nums[j])==k){
-                    pairCount++;
-                }
-            }
+        // int n =nums.length;
+        // for(int i=0;i<n-1;i++){
+        //     for(int j = i+1;j<n;j++){
+        //         if(Math.abs(nums[i]-nums[j])==k){
+        //             pairCount++;
+        //         }
+        //     }
+        // }
+
+
+        //Another optimal solution is to use the fact that i+j=k => j=i-k for i>=k
+        //array to store frequency of occurrence of nums, 1 <= nums[i] <= 100
+        int[] frequency = new int[101]; //disregard 0, we need 100 more so 101
+        //update the frequency
+        for(int n:nums){
+            frequency[n]++;
         }
+
+        for(int i=k;i<=100;i++){
+            pairCount+= frequency[i] * frequency[i-k]; //frequency of i * frequency of j (j=i-k) will give the total pairs possible with difference =k
+        }
+
 
         return pairCount;
     }
