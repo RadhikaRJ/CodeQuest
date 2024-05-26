d081db4babb74fe60825d862f3bb880182d63b71
Radhika Joshi <radhika7002@gmail.com>
2024-05-26 12:58:26 -0700

    Time: 1 ms (80.38%), Space: 41.1 MB (88.04%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,38 +1,24 @@
 class Solution {
-    
-    Set<Integer> daysToTravel = new HashSet<>();
     public int mincostTickets(int[] days, int[] costs) {
         int lastDay = days[days.length-1];
-        int[] dp = new int[lastDay+1];
-        Arrays.fill(dp,-1);
+        int dp[] = new int[lastDay+1];
+        Arrays.fill(dp,0);
 
-        for(int day:days){
-            daysToTravel.add(day);
+        int i=0;
+        for(int day=1;day<=lastDay;day++){
+            if(day<days[i]){
+                dp[day]=dp[day-1];
+            }
+            else{
+                i++;
+                dp[day] = Math.min(dp[day-1]+costs[0], 
+                Math.min(
+            dp[Math.max(0,day-7)]+costs[1],
+             dp[Math.max(0,day-30)]+costs[2]));
+
+            }
         }
 
-        return findMinCost(days,costs,dp,1);
-        
-    }
-
-    private int findMinCost(int[] days, int[] costs, int[] dp, int currentDay){
-
-        if(currentDay>days[days.length-1]){
-            return 0;
-        }
-
-        if(!daysToTravel.contains(currentDay)){
-            return findMinCost(days,costs,dp,currentDay+1);
-        }
-
-        if(dp[currentDay]!=-1){
-            return dp[currentDay];
-        }
-
-        int oneDayCost = costs[0]+findMinCost(days,costs,dp,currentDay+1);
-        int sevenDayCost = costs[1]+findMinCost(days,costs,dp,currentDay+7);
-        int thirtyDayCost = costs[2]+findMinCost(days,costs,dp,currentDay+30);
-
-        dp[currentDay] = Math.min(oneDayCost,Math.min(sevenDayCost,thirtyDayCost));
-        return dp[currentDay];
+        return dp[lastDay];
     }
 }
