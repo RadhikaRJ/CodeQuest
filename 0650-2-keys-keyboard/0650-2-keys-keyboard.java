fec9dcb54497ed0c44c40eaf5418014ea33a3cc0
Radhika Joshi <radhika7002@gmail.com>
2024-05-27 12:50:12 -0700

    Time: 0 ms (100%), Space: 40.1 MB (82.31%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minSteps(int n) {
+        int ans = 0;
+        for(int i=2;i*i<=n;){
+            if(n%i==0){
+                ans = ans+i;
+                n=n/i;
+            }
+            else{
+                i++;
+            }
+        }
+
+        if(n!=1){
+            ans=ans+n;
+        }
+
+        return ans;
+    }
+}
