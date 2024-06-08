833fef951bfff2c977258fd043930075d5ccab1b
Radhika Joshi <radhika7002@gmail.com>
2024-06-08 15:32:52 -0700

    Time: 2 ms (92.58%), Space: 57 MB (54.85%) - LeetHub

---
+++ 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumSwaps(int[] nums) {
+     //  [3,4,5,5,3,1 ]
+     //  [0,1,2,3,4,5 ]
+     //  [1,3,4,5,3,5 ]
+
+    int maxPosition = nums.length-1, minPosition = 0;
+    int currentMaxIndex = 0, currentMinIndex = 0;
+    int totalSwaps = 0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[currentMaxIndex]<=nums[i]){
+            currentMaxIndex = i;
+        }
+        if(nums[currentMinIndex]>nums[i]){
+            currentMinIndex = i;
+        }
+    }
+
+   int  swaps = currentMinIndex;
+    swaps+=nums.length-currentMaxIndex-1;
+    if(currentMinIndex>currentMaxIndex){
+        swaps-=1;
+    }
+
+    return swaps;
+
+
+    }
+}
